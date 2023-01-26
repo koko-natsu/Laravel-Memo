fddb0e624ba7cb93959bfc9c99b3472e70e863b5
yuta higa <h.yuta2314@gmail.com>
2023-01-26 20:41:50 +0900

    Modified Tags migration file

--- database/migrations/2023_01_26_082514_create_tags_table.php
+++ database/migrations/2023_01_26_082514_create_tags_table.php
@@ -14,12 +14,16 @@ class CreateTagsTable extends Migration
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('id', true);
+            $table->string('name');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRNET_TIMESTAMP'));
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
-    /**
+    /** 
      * Reverse the migrations.
      *
      * @return void
